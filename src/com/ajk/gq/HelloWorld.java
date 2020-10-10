267aa2e054d5187e2d09b730b139227375d0374a
golore <golore@163.com>
2020-10-10 10:59:35 +0800

    first commit

--- src/com/ajk/gq/HelloWorld.java
+++ src/com/ajk/gq/HelloWorld.java
@@ -2,6 +2,22 @@ package com.ajk.gq;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        float a=0;
+        int b=0;
+
+        System.out.print(a==b);
+        for(int i=0;i<2;i++){
+            for (int j=0;j<10000000;j++){
+                a++;
+                b++;
+            }
+            System.out.println("-----");
+            System.out.println(a == b);
+
+            System.out.println(a);
+            System.out.println(b);
+        }
+
     }
 }
